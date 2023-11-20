f5f94e6000a6c8752c9a0fd9020a4ccf7bc58653
Ankur4591 <ankur.verma@repolite.com>
2023-11-20 13:52:37 +0530

    Minor Changes

--- src/Messages/message.controller.ts
+++ src/Messages/message.controller.ts
@@ -1,4 +1,28 @@
-import { Controller } from '@nestjs/common';
+import { Controller,Get,Put,Delete,Post,Param} from '@nestjs/common';
+import { MessageService } from './message.service';
+
 
 @Controller('message')
-export class MessageController {}
+export class MessageController {
+
+    constructor(private msg:MessageService){}
+
+    @Get()
+    async getAll(){
+        return this.msg.getAllMsg()
+    }
+
+    @Post(":id")
+    async getSingleMsg(@Param("id") id:number){
+        return this.msg.getSingleMessage(id)
+    }
+
+    @Delete(":id")
+    async deleteOne(@Param("id") id:number){
+       
+        
+
+    }
+
+
+}
